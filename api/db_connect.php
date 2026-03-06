ba39b11b51588dc3fb8d81c9fdde09eb87dd9da6
Jiradech ksr <jiradech.ksr@gmail.com>
2026-03-06 15:51:00 +0700

    modified:   api/db_connect.php

--- api/db_connect.php
+++ api/db_connect.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "campus_nav";
+$dbname = "universitynavigation_db";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
